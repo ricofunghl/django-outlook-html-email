c6ac99e44e20e944e0aeea4bcc0ba55446af18fb
ricofunghl <rico.fung.hl@gmail.com>
2020-03-21 23:38:57 -0600

    added emailer and methods and email template

--- html_email/emailer.py
+++ html_email/emailer.py
@@ -1,18 +1,31 @@
 import win32com.client as win32
 from django.contrib.staticfiles import finders
+from django.template.loader import get_template
 
-def sendEmail(subject, to, body):
+from html_email.models import Fruit
+
+
+def send_email(body):
     olMailItem = 0x0
     outlook = win32.gencache.EnsureDispatch('Outlook.Application')
     new_mail = outlook.CreateItem(olMailItem)
 
 
-    attachment = new_mail.Attachments.Add(finders.find('images\\attestation.gif'), win32.constants.olEmbeddeditem, 0,
+    attachment = new_mail.Attachments.Add(finders.find('images\\fruit.jpg'), win32.constants.olEmbeddeditem, 0,
                                          "Attestation")
-    imageCid = "attestation.gif@123"
+    imageCid = "fruit@image"
     attachment.PropertyAccessor.SetProperty("http://schemas.microsoft.com/mapi/proptag/0x3712001E", imageCid)
 
     new_mail.Subject = subject
     new_mail.To = to
-    new_mail.HTMLBody = body.replace('attestation_gif_src',imageCid)
+    new_mail.HTMLBody = body.replace('fruit_img_src',imageCid)
     new_mail.Send()
+
+def construct_email_body():
+    fruits = Fruit.objects.all()
+    email_temp = get_template('email_html.html')
+    email_ctx = dict({'fruits': fruits,})
+
+    body = email_temp.render(email_ctx)
+
+    send_email(body)
